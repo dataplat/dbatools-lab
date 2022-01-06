66db45922b36385c23acd3479d7bba38daccb9aa
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-06 17:19:21 +0000

    chapter 6

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -11,7 +11,7 @@ BeforeDiscovery {
     }
 }
 
-Describe "Checking the file <_.Name> code works as intended" -ForEach $files[0..4] {
+Describe "Checking the file <_.Name> code works as intended" -ForEach $files[5] {
     $filename = $_.Name
 
     It "The code <_> should not error"  -ForEach @($tests | Where-Object { $_.FileName -eq $filename }).Code {
