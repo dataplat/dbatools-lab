839b662a0d6219dfe5efa5709d867476686a3559
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-13 10:47:02 +0000

    this makes it break so we wont run it

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -110,7 +110,8 @@ Describe "Checking the file <_.Name> code works as intended" -ForEach $files[0..
             '11112019080741',
             'DetachAttach  = $true',
             'Copy-DbaLinkedServer',
-            'Remove-DbaDbTable'
+            'Remove-DbaDbTable',
+            'ad\factoryauditors'
 
         )
         #find if it matches and write it out so we see it in the output and know it was looked at
