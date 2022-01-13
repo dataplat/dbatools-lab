ed79df0c7591580315459dcaeb07a9da9658d7be
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-13 15:05:20 +0000

    ch19

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -46,7 +46,7 @@ WarningAction = ''SilentlyContinue'''  -replace  '\$tableSplat = @{
     }
 }
 
-Describe "Checking the file <_.Name> code works as intended" -ForEach $files[15..17] {
+Describe "Checking the file <_.Name> code works as intended" -ForEach $files[18] {
     $filename = $_.Name
 
     It "The code <_> should not error"  -ForEach @($tests | Where-Object { $_.FileName -eq $filename }).Code {
