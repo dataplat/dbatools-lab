de5f437e28f57d75b436ee11d51275f85a79e945
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-14 12:39:47 +0000

    cert - ainly not on LInux

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -131,7 +131,8 @@ Describe "Checking the file <_.Name> code works as intended" -ForEach $files[22]
             'Tracker.xml',
             'Watch-DbaXESession',
             'Read-DbaXEFile',
-            'XESessionTemplate'
+            'XESessionTemplate',
+            'Set-DbaNetworkCertificate'
 
         )
         #find if it matches and write it out so we see it in the output and know it was looked at
