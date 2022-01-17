0b64eefaba56a123765e011930e93e15ad0cee1d
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-17 10:55:15 +0000

    back to ch23

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -53,7 +53,7 @@ DecryptionPassword = $sqlcred.Password' -replace 'New-DbaDbMasterKey @params','N
     }
 }
 
-Describe "Checking the file <_.Name> code works as intended" -ForEach $files[26] {
+Describe "Checking the file <_.Name> code works as intended" -ForEach $files[22] {
     $filename = $_.Name
 
     It "The code <_> should not error"  -ForEach @($tests | Where-Object { $_.FileName -eq $filename }).Code {
