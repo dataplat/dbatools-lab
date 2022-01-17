14fb7471eaa6660b24303aebbc4292accbd4edb6
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-17 16:35:29 +0000

    because it takes sooooo  long

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -186,7 +186,8 @@ Describe "Checking the file <_.Name> code works as intended" -ForEach $files {
             'Set-DbaExtendedProtection',
             'Enable-DbaHideInstance',
             'SET ENCRYPTION ON',
-            'Dbc'
+            'Dbc',
+            'PiiScan'
 
         )
         #find if it matches and write it out so we see it in the output and know it was looked at
