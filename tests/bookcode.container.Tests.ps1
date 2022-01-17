d9acc5c1dd37aef2d925dc639d0fc8427018f056
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-01-17 10:24:05 +0000

    aint no way we are doing that - just wait for hte update to the main branch of the book

--- tests/bookcode.container.Tests.ps1
+++ tests/bookcode.container.Tests.ps1
@@ -43,8 +43,8 @@ WarningAction = ''SilentlyContinue'''  -replace  '\$tableSplat = @{
 \s+Start-DbaXESession' , ' Select *  # Start-DbaXESession' -replace '\s+Source = "mssql1"
 \s+Destination "mssql2", "mssql3"', '    Source = ''localhost,15592''
 Destination = ''localhost,15593''' -replace ([Regex]::Escape('(Get-Credential doesntmatter).Password')),'$sqlcred.Password' -replace ([Regex]::Escape('EncryptionPassword = $securepass')),'EncryptionPassword = $sqlcred.Password
-DecryptionPassword = $sqlcred.Password' -replace 'New-DbaDbMasterKey @params','New-DbaDbMasterKey @params -Confirm:$false' -replace 'c:\\backups','/tmp' -replace 'c:\\backups\\','/tmp/' -replace 'EncryptionAlgorithm = AES192', ' EncryptionAlgorithm = ''AES192''' -replace 'Set-DbaDbCompression @splatSetCompression
-', 'Set-DbaDbCompression @splatSetCompressionRow'
+DecryptionPassword = $sqlcred.Password' -replace 'New-DbaDbMasterKey @params','New-DbaDbMasterKey @params -Confirm:$false' -replace 'c:\\backups','/tmp' -replace 'c:\\backups\\','/tmp/' -replace 'EncryptionAlgorithm = AES192', ' EncryptionAlgorithm = ''AES192''' 
+
 
         [PSCustomObject]@{
             FileName = $file.Name
